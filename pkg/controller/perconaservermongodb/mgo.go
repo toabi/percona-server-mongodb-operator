4166a2e20a1984bbc0e3345948c03bb954f7dc6a
Tobias Birmili <birmili@mecodia.de>
2020-10-06 16:03:43 +0200

    fix: try sth. else, again, and again, and again

--- pkg/controller/perconaservermongodb/mgo.go
+++ pkg/controller/perconaservermongodb/mgo.go
@@ -257,7 +257,7 @@ func (r *ReconcilePerconaServerMongoDB) handleReplsetInit(m *api.PerconaServerMo
 
 		cmd[2] = fmt.Sprintf(
 			`
-			cat <<-EOF | mongo -u ${MONGODB_USER_ADMIN_USER} -p ${MONGODB_USER_ADMIN_PASSWORD} --host %s %s"
+			cat <<-EOF | mongo -u ${MONGODB_USER_ADMIN_USER} -p ${MONGODB_USER_ADMIN_PASSWORD} --host %s %s
 			%s
 			EOF
 			`, host, tlsConnectionParameter, mongoInitUsers) //FIXME: we removed replicaSet here?!
